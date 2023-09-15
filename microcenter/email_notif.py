521d1074866ce6f99f239618228fde95cca2d511
MrLately <94589563+MrLately@users.noreply.github.com>
2023-09-15 13:02:30 -0400

    Update email_notif.py

--- microcenter/email_notif.py
+++ microcenter/email_notif.py
@@ -10,7 +10,14 @@ def send_notification(items_availability):
     yag = yagmail.SMTP(user=user, password=sender_password)
     subject = "Product Availability Notification"
     body = ""
-    for item_name, availability_status in items_availability.items():
-        body += f"The {item_name} is {availability_status}.\n"
+    for item_name, availability_info in items_availability.items():
+        availability_status = availability_info['availability_status']
+        inventory_count = availability_info['inventory_count']
+        
+        if availability_status == "In stock!":
+            body += f"{availability_status} : {item_name} : {inventory_count} available\n"
+        else:
+            body += f"{availability_status} : {item_name}\n"
+    
     yag.send(to=email_receiver, subject=subject, contents=body)
     print("Sent notification")
